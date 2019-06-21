668cd4be0c93bfc9b489ccf3bebd6b1ada49d3fb
Sherri Mitchell <rsamitchell@att.net>
2019-06-21 19:37:47 -0400

    added author model, added belongs to association to books, migration files, updated schema, author spec file

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_183935) do
+ActiveRecord::Schema.define(version: 2019_06_21_232541) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
 end
